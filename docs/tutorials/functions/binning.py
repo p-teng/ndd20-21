11dcf6b4faa01bd4458d0185d9ece09001259ece
fteng3 <fteng3@jhu.edu>
2020-10-21 11:32:54 -0400

    Binning update

--- docs/tutorials/functions/binning.py
+++ docs/tutorials/functions/binning.py
@@ -4,10 +4,9 @@ from sklearn.preprocessing import KBinsDiscretizer
 
 def KBinsDiscretize(data_x, n_bins=0, encode="ordinal", strategy="uniform"):
    # Makes n_bins optional, calculates optimal n_bins by default
-   # Sturge's Rule - num_bins = 1+log_2(num_features)
+   # Sturge's Rule - num_bins = 1+log_2(num_inputs)
    if n_bins == 0:
       n_bins = math.floor(1 + math.log(data_x.shape[0], 2))
-   print(n_bins)
    binner = KBinsDiscretizer(n_bins, encode="ordinal", strategy="uniform")
    binner.fit(data_x)
    binned_x = binner.transform(data_x)
